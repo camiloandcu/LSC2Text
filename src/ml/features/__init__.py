cde5f71557c0134261ceff90fca24f90c25d773c
andrescuel <camilo.andcu@gmail.com>
2026-05-04 17:37:51 -0500

    feat (ml): add feature extraction with HOG and LBP

---
+++ src/ml/features/__init__.py
@@ -0,0 +1,5 @@
+"""Feature extraction utilities."""
+
+from .feature_extraction import FeatureConfig, HogConfig, LbpConfig, extract_features
+
+__all__ = ["FeatureConfig", "HogConfig", "LbpConfig", "extract_features"]
